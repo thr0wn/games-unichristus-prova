7294b7e575cf3d0e7d8dd93394afe74f18f1da50
Natan Camargos <natanscamargos@gmail.com>
2016-05-01 19:57:55 -0300

    atualizando personagens

--- public/src/constants/characters.js
+++ public/src/constants/characters.js
@@ -3,37 +3,43 @@ angular.module('gu')
 		{
 			name: 'Amana',
 			nameOrigin: 'Amana, do tupi, água que vem do céu. Referências: Tenshi no Tamago, seapunk art',
-			desc: 'Amana personagem principal da trama ela vive em uma sociedade contemporânea e é totalmente alheia aos acontecimentos da sociedade em que vive se preocupando mais consigo mesmo do que com os outros.Tudo isso muda quando eventos surreais e catastróficos começam a acontecer e ela recebe uma visita de Obatalá que a convencer que não só ela está correndo perigo, como também todo o universo e outros universos que ela nem sabia que Nômade, mundo de ruínas, água que toma conta de tudo.',
+			desc: 'Amana, personagem principal da trama, é uma jovem adulta tendo em torno de 20 anos. Apática em relação à sociedade, não tem muitos amigos e trabalha no conserto e melhoramento de barcos, sendo muito talentosa no que faz. Por uma discussão com seu chefe e questioná-lo por querer enganar um cliente, é demitida. Neste dia à noite, vai ao bar e à partir daí tudo isso muda devido aos eventos surreais e catastróficos que ocorrem devido às consequências do cataclismo. Durante o caos causado pela tsunami, o “Mentor” à visita em forma etérea e a guia para um portal que à leva para Summa. Lá, ele a convence que não só ela está correndo perigo, como também todo o seu mundo e outros mundos que ela nem sabia que existia.',
 			image: 'imgs/characters/1.jpg'
 		},
 		{
 			name: 'Marie Frank/Rasga­-Mortalha',
 			nameOrigin: 'Referências: Marie Curie,Frankenstein, Batman, Fullmetal alchemist.',
-			desc: 'Seus pais foram extraordinários alquimistas que ficaram milionários ao descobrir a técnica de converter metais em ouro. Tudo mudou para a jovem Marie quando seu pai faleceu de uma doença misteriosa. Sua mãe, inconformada, tentou quebrar o tabu da ressurreição de corpos humanos e acabou desaparecendo no processo do ritual alquímico. Depois desses acontecimentos, Marie busca aperfeiçoar seus conhecimentos em alquimia e investigação. Vendo que essa investigação traria riscos à sua vida, Marie adota a identidade de Rasga­Mortalha, munindo­se de equipamentos e feitiços de alquimia.',
+			desc: 'Seus pais foram extraordinários alquimistas que ficaram milionários ao descobrir a técnica de converter metais em ouro. Tudo mudou para a jovem Marie quando seu pai faleceu de uma doença misteriosa. Sua mãe, inconformada, tentou quebrar o tabu da ressurreição de corpos humanos e acabou desaparecendo no processo do ritual alquímico. Depois desses acontecimentos, Marie busca aperfeiçoar seus conhecimentos em alquimia e investigação. Vendo que essa investigação traria riscos à sua vida, Marie adota a identidade de Rasga-Mortalha, munindo-se de equipamentos e feitiços de alquimia. O mundo que vive (Corporativo) é muito poluído, a sociedade é muito opressora  e Marie luta contra essas duas coisas tanto politicamente quanto em combates utilizando o alter-ego Rasga Mortalha.',
 			image: 'imgs/characters/2.jpg'
 		},
 		{
 			name: 'Tenma',
 			nameOrigin: 'Demônio dos Céus. Referências: Vaatu ­ Legend of Korra',
-			desc: 'Tenma foi um monge sábio e grande virtude do mundo primordial. Seria um dos primeiros mortais a transcender ao Mundo Fora da Árvore, porém no último momento foi vencido pelo demônio da vaidade. Após entrar em profunda depressão, Tenma resolveu descarregar sua frustação dedicando­se a destruir a Ávore Yggdrasil. Por ser um ser de imenso poder, mesmo não tendo sucesso em sua missão, conseguiu desferir um golpe que abalou as estruturas da árvore. Após esse evento, Tenma foi capturado e selado dentro das florestas do vale do crepúsculo, numa árvore de galhos retorcidos e espinhosos.',
+			desc: 'Tenma foi um monge sábio e grande virtude do mundo primordial. Foi o primeiro e único mortal a iniciar a ascenção ao Summa sem necessitar dos Antedeluvianos, porém no momento de transição foi vencido pelo “demônio” da vaidade. Após entrar em profunda depressão, Tenma enlouquece, acreditando que a pode alcançar transcendência dedicando-se a destruir a “Árvore Eterna” e o Druida. Por ser um ser de imenso poder, mesmo não tendo sucesso em sua missão, conseguiu desferir um golpe que feriu mortalmente o Druida, abalando assim as estruturas da árvore. Após esse evento, Tenma foi capturado e selado pelo Druida dentro das florestas do vale do crepúsculo, numa árvore de galhos retorcidos e espinhosos.',
 			image: 'imgs/characters/3.jpg'
 		},
 		{
 			name: 'Obatalá',
 			nameOrigin: 'Referências: Raava ­Legend of Korra',
-			desc: 'Um dos seres de do Mundo Fora da Árvore, Obatalá foi um dos seres que participaram na criação da árvore Yggdrasil. Ele é o responsável por ajudar os heróis da aventura a saberem o que precisam fazer para cumprirem seus objetivos e missões por onde terão que passar por obstáculos e testes.',
+			desc: 'Um dos Antedeluvianos, o Mentor foi um dos seres que participaram na criação da “Árvore Eterna”. Ele é o responsável por ajudar e guiar Amana durante o jogo. Possui conhecimento sobre todos os mundos e todos os seres. Define os objetivos e dá o direcionamento necessário para que os objetivos e missões sejam cumpridos.',
 			image: 'imgs/characters/4.jpg'
 		},
 		{
-			name: 'Nílbio',
-			nameOrigin: 'Referências: Constantine?',
-			desc: 'Nilbio, foi retirado de um dos milhares de universos menores que que existem nos pequenos ramos e bifurcações da yggdrasil, chamada Terra,depois do colapso que yggdrasil sofreu ele perdeu toda suas memorias de vivencia anterior, foi levado ao mundo corporativista, nele ele se tornou uma das maiores influencias e ao longo desse período ele acendeu a liderança e começou à controlar a todos com suas leis e regras rígidas.',
+			name: 'b4b4_j4g3r',
+			nameOrigin: 'Referências: Baba Yaga, trickster, Han Solo',
+			desc: 'b4b4_j4g3r, um dos melhores hackers do mundo Tecnológico, porém não é uma pessoa confiável. Conhecido por trabalhar apenas em favor próprio, não mede esforços para lucrar o máximo possível com seus serviços. Por vezes engana o próprio contratante, roubando seu dinheiro apenas para gastar em seguida em festas e bebida. Possuidor de personalidade excêntrica, é paranóico e fervorosamente contra o uso de melhoramentos tecnológicos em si próprio por medo de que terceiros consigam hackear seu corpo.',
 			image: 'imgs/characters/5.jpg'
 		},
 		{
-			name: 'Thor',
-			nameOrigin: 'Referências: Summon de Final Fantasy?',
-			desc: 'Thor è a divindade que protege e controla o mundo teocrático onde todas as pessoas são submissas fortemente ao conceito da religiosidade e da fé inquebrável onde quase todos são fiéis a Thor e suas leis, apesar de algumas ressurgentes aparições de cultos a deuses pagãos, todos vivem em harmonia, até o inicio do cataclismo da yggdrasil, na qual abalou todos os mundos, e sua ordem fora corrompida por uma grande maldade insurgente no corações de seus fiéis que começaram a blasfemar uns contra os outro e contra sua própria santidade.',
+			name: 'Patrus',
+			nameOrigin: 'Referências: Faraó, Rá, Odin, Javé, Perun, Zeus',
+			desc: 'Patrus é a divindade principal do panteão do mundo teocrático, sendo o patriarca dos outros deuses. Deus da força e do trabalho, o martelo simboliza seus elementos. A sociedade do mundo Teocrático é culturalmente forte adepta do conceito da religiosidade e da fé inquebrável onde quase todos são fiéis a Patrus e suas leis. Outras divindades do panteão também são cultuadas e aceitas. Todos vivem em relativa harmonia, até o inicio do cataclismo da “Árvore Eterna”. À partir daí, fiéis de deuses diferentes culparam a fé alheia pelas catástrofes naturais, causando conflitos entre diversos segmentos da sociedade.',
 			image: 'imgs/characters/6.jpg'
-		}
+		},
+		{
+			name: 'Druida',
+			nameOrigin: 'Guardião da Árvore Eterna. Referências: indígenas norte americanos',
+			desc: 'O Druida atual é um ser milenar, tendo seu nome já perdido nas eras. Escolhido pelos Antedeluvianos por sua sabedoria e tenacidade, o seu mundo de origem era um mundo onde as pessoas viviam em uma sociedade pacífica e equilibrada, onde a natureza era considerada divina e se acreditava no espírito das coisas. Após o conflito com Tenma, ficou mortalmente ferido, conseguindo apenas aprisioná-lo e depois voltar para seu mundo de origem para morrer em paz. Devido à natureza do seu ferimento, porém, ele ficou inconsciente e está agonizando. Devido ao poder que ele acumulou durante tanto tempo, suas alucinações moribundas se tornam realidade e permeiam seu mundo de origem, que se tornou completamente caótico e macabro.',
+			image: 'imgs/characters/7.jpg'
+		},
 	]);
